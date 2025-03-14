5d64a63f9d56232706ee31101516aa7ec2c586b1
Shuvra Saha <88980577+shuvrasaaha@users.noreply.github.com>
2025-03-14 15:26:08 +0600

    Update index.js

--- index.js
+++ index.js
@@ -43,6 +43,6 @@ app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
 
-//To see how the final website should work, run "node solution.js".
+//To see how the final website should work, run "node index.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
